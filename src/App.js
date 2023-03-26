6d397dd7405079d7a320da624b5f594c1cae98ad
ndtfat <110542869+ndtfat@users.noreply.github.com>
2023-03-26 08:12:42 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <audio
+                autoPlay
+                controls
+                // hidden
+                src="https://cf-media.sndcdn.com/nIS8vzW8FwmA.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vbklTOHZ6VzhGd21BLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjc5NzYwODUzfX19XX0_&Signature=Fa2zDdvelQHzqvx-kO8dgiDg0tcd7DkXsC6qs~mCL4cuDUF~VI28fDnu~JaxnXUFJbr17w5j2Y1nV~hRXOu4HgoFrgQDAwSUT-j2~2eF~6hKgjoMj00P4DvtbOPK9uMP-fBCqa~QOO0wBbmRG7muR~LMd4o65msH80uV1Tkk0QEZNn2Clj2oHAwK3Me0k7tipZznu7SzzJTcBK7umvIEG7oYn~l9qOqeh9~RGENORToB2LcDLr2TwArDfSu4F3zHbCp2qkGht2f34qudek8mp1ySu0iWk-ZTRM8In58y~mssVDQrst0QYRxHyeSYB35A2y41bkBt4tYe1m6yvW3k3Q__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ"
+            ></audio>
+        </div>
+    );
 }
 
 export default App;
