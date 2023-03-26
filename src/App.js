dc77cd9d54bb3483e83000533e4358eb9a87d79c
ndtfat <110542869+ndtfat@users.noreply.github.com>
2023-03-26 08:58:27 +0700

    test createURL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+    const [songList, setSongList] = useState([]);
+
+    const handleUpfile = (e) => {
+        const file = URL.createObjectURL(e.target.files[0]);
+        console.log('file: ', file);
+    };
+
+    useEffect(() => {
+        fetch('https://64183e4429e7e36438e31657.mockapi.io/api/songs')
+            .then((res) => res.json())
+            .then((songs) => {
+                console.log(songs);
+                setSongList(songs);
+            });
+    }, []);
+
     return (
         <div className="App">
-            <audio
-                autoPlay
-                controls
-                // hidden
-                src="https://cf-media.sndcdn.com/nIS8vzW8FwmA.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vbklTOHZ6VzhGd21BLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjc5NzYwODUzfX19XX0_&Signature=Fa2zDdvelQHzqvx-kO8dgiDg0tcd7DkXsC6qs~mCL4cuDUF~VI28fDnu~JaxnXUFJbr17w5j2Y1nV~hRXOu4HgoFrgQDAwSUT-j2~2eF~6hKgjoMj00P4DvtbOPK9uMP-fBCqa~QOO0wBbmRG7muR~LMd4o65msH80uV1Tkk0QEZNn2Clj2oHAwK3Me0k7tipZznu7SzzJTcBK7umvIEG7oYn~l9qOqeh9~RGENORToB2LcDLr2TwArDfSu4F3zHbCp2qkGht2f34qudek8mp1ySu0iWk-ZTRM8In58y~mssVDQrst0QYRxHyeSYB35A2y41bkBt4tYe1m6yvW3k3Q__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ"
-            ></audio>
+            <input type="file" onChange={handleUpfile}></input>
+            {songList.map((song) => (
+                <div key={song.id}>
+                    <p>{song.name}</p>
+                    <p>{song.performer}</p>
+                    <audio controls src={song.songURL}></audio>
+                </div>
+            ))}
         </div>
     );
 }
