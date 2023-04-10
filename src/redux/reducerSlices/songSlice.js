38aa389aa38fcd3ea9d22a7be1b354a68b81ee4f
ndtfat <110542869+ndtfat@users.noreply.github.com>
2023-04-10 23:32:04 +0700

    fix css range on edge

--- src/redux/reducerSlices/songSlice.js
+++ src/redux/reducerSlices/songSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { act } from 'react-dom/test-utils';
 import { playlists } from '../../assets';
 
 const initialState = localStorage.getItem('song')
